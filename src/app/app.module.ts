4fc6023e2301fcd5d141b6ed911a7568cbf4a62d
Lorenzo Trabbia <lorenzo.trabbia@edu.itspiemonte.it>
2022-10-10 12:58:58 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,20 @@ import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { AngularMaterialModule } from 'src/module/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    ProfileComponent
-  ],
+  declarations: [AppComponent, LoginComponent, HomeComponent, ProfileComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    AngularMaterialModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
