e6d5d65518f6bc0c481b923f593b828cf3b190e2
Lorenzo Trabbia <lorenzo.trabbia@edu.itspiemonte.it>
2022-10-12 11:53:57 +0200

    update

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,27 +5,72 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  jsonIn = {
-    username: '',
-    password: '',
-    rememberMe: false,
-  }
-  @ViewChild('loginForm') loginForm!: NgForm;
-  showErrors = false;
-  showPassword = false;
-  
+  jsonInFisica = {
+    nome: '',
+    cognome: '',
+    email: '',
+    telefono: '',
+    maschio: 'maschio',
+    femmina: '',
+    cod_fiscale: '',
+    tipo_indirizzo: 'via',
+    indirizzo: '',
+    comune: '',
+    provincia: '',
+    cap: '',
+    pec: '',
+    fax: '',
+    maggiorenne: false,
+    prodotto_auto: false,
+    prodotto_vita: false,
+    prodotto_salute: false,
+  };
+
+  jsonInGiuridica = {
+    nome: '',
+    cognome: '',
+    email: '',
+    telefono: '',
+    maschio: 'maschio',
+    femmina: '',
+    cod_fiscale: '',
+    tipo_indirizzo: 'via',
+    indirizzo: '',
+    comune: '',
+    provincia: '',
+    cap: '',
+    pec: '',
+    fax: '',
+    maggiorenne: false,
+    prodotto_auto: false,
+    prodotto_vita: false,
+    prodotto_salute: false,
+  };
+
+  @ViewChild('loginFormFisica') loginFormFisica!: NgForm;
+  showErrorsFisica = false;
+
+  @ViewChild('loginFormGiuridica') loginFormGiuridica!: NgForm;
+  showErrorsGiuridica = false;
+
   constructor(private router: Router) {}
 
   signIn() {
-    if (this.loginForm.form.invalid) {
-      this.showErrors = true;
+    if (this.loginFormFisica.form.invalid) {
+      this.showErrorsFisica = true;
     } else {
-      this.router.navigateByUrl('/home');
+      console.log(this.jsonInFisica.maschio);
     }
   }
 
-
+  signInGiuridica() {
+    if (this.loginFormGiuridica.form.invalid) {
+      this.showErrorsGiuridica = true;
+    } else {
+      console.log(this.jsonInGiuridica.maschio);
+    }
+  }
 }
